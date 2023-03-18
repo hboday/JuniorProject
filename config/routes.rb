9875dd83a3a28b810fbe4196127a7da0dd6c4b05
Hessa Boday <hboday@andrew.cmu.edu>
2023-03-19 00:52:37 +0300

    Updated Demo 2

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-
+ 
   resources :employees, only: [:show]
 
   #resources :shift_assignments do
